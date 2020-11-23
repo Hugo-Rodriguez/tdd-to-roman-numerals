1cd9d09ba5ea8fc8b5d84a43332baebc48675711
Hugo Rguez <HugoRguez@example.com>
2020-11-23 11:06:23 +0100

    commit 2

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
-
-
 /**
  * Conversión a números romanos.
  * @param {number} n - Un número entre 1 y 3999.
  * @returns {string} El equivalente en números romanos.
  */
 export function toRoman(n) {
- if (n==1) return "I";
- if (n==2) return "II";
+  let resultado = "";
+
+  if (n == 4) {
+    resultado = "IV";
+  } else {
+    if (n == 5) {
+      resultado = "V";
+    } else {
+      for (let i = 0; i < n; i++) {
+        resultado += "I";
+      }
+    }
+  }
+
+  return resultado;
 }
-
-
